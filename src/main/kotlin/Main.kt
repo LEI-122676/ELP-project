e2ee4a002a480e009780126d9ab8077a313ee6e8
tigago <tiago.freitas.devs@gmail.com>
2026-05-01 15:53:52 +0100

    fix: JSON grammar

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,14 @@
 package org.example
 import java.io.File
 
+//
 fun main(args: Array<String>) {
-    // instancia template dado o argumento nome do ficheiro template
 
-
-    if (args.isEmpty()) {
-        println("error: esse comando não estáa certo")
-        return
-    }
-    // engine.kt -f "template.txt" -i "input.json" -o "result.txt"
-    if (args.size != 6) {
-        println("error: not right amount of arguments")
-        return
+    if (args.isEmpty() || args.size != 6) {
+        throw IllegalArgumentException("Argumentos estão mal.\nExemplo: engine.kt -f template.txt -i input.json -o result.txt\n")
     }
 
-    val template = args[args.indexOf("-f") + 1]
+    val template = args.get(args.indexOf("-f") + 1)
     val input = args.get(args.indexOf("-i") + 1)
     val output = args.get(args.indexOf("-o") + 1)
 
